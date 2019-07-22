8935ecd6b459e9c50c43421423d51a0778ebf3ea
Abdelrahman <abdelrahmanlotfy606@gmail.com>
2019-07-22 02:09:43 +0200

    create drawer

--- news_app/lib/shared_ui/shared-drawer.dart
+++ news_app/lib/shared_ui/shared-drawer.dart
@@ -6,10 +6,28 @@ class SharedDrawer extends StatefulWidget {
 }
 
 class _SharedDrawerState extends State<SharedDrawer> {
+
+  List<String> navs = [
+    'Explore',
+    'Headine News',
+    'Read Later',
+    'Videos',
+    'Photos',
+    'Settings',
+    'Log Out'
+  ];
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      
+      child: Container(
+        padding: EdgeInsets.only(left: 15, right: 15, top: MediaQuery.of(context).size.height * .18),
+        child: ListView.builder(itemBuilder: (context, index){
+          return ListTile(
+            title: Text(navs[index], style: TextStyle(color: Colors.black.withOpacity(.6), fontSize: 20),),
+            trailing: Icon(Icons.chevron_right, size: 30,),
+          );
+        }, itemCount: navs.length,),
+      ),
     );
   }
 }
