14a5943854185098496b72edea78bf3fdecfb911
= <=abdoelsimy123@gmail.com>
2019-08-06 19:28:48 +0200

    work onn single page post

--- news_app/lib/screens/single_page.dart
+++ news_app/lib/screens/single_page.dart
@@ -12,11 +12,49 @@ class _SinglePostPageState extends State<SinglePostPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      
       body: Container(
-        child: Text(widget.post.title),
-        color: Colors.red,
+        child: Stack(
+          children: <Widget>[
+            CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  actions: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: (){},
+                    )
+                  ],
+                  centerTitle: true,
+                  backgroundColor: Colors.red,
+                  expandedHeight: MediaQuery.of(context).size.height * .3,
+                  floating: true,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Image.network(widget.post.featuredImage,
+                        fit: BoxFit.cover),
+                    title: Container(
+                      margin: EdgeInsets.only(right: 100),
+                      child: Text(widget.post.title, style: TextStyle(fontSize: 16),),
+                    ),
+
+                  ),
+                ),
+                SliverFixedExtentList(
+                  itemExtent: 1000.0,
+                  delegate: SliverChildListDelegate(
+                    [
+                      Container(
+                        height: 1000,
+                        color: Colors.red,
+                        child: Text('asdlkjdsjasdklj'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
